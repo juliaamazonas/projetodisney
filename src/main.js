e5e77c91ba655fc8c146f8e1f40927f20eb075b2
Julia Alvim <juliameuportoseguro@gmail.com>
2024-04-11 20:18:08 -0300

    ajuste no mobile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+    
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
 
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual < alturaHero) {
+            ocultaElementosdoHeader();
+        } else {
+            exibeElementosdoHeader();
+        }
+    })
+
+
+
+    // Seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -10,24 +27,43 @@ document.addEventListener('DOMContentLoaded', function() {
             removeBotaoAtivo();
             botao.target.classList.add('shows__tabs__button--is-active');
         })
-    
     }
 
+    function removeBotaoAtivo () {
+        const buttons = document.querySelectorAll('[data-tab-button]');
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].classList.remove('shows__tabs__button--is-active')
+        }
+    }
+    
+        function escondeAbas () {
+        const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    
+        for (let i = 0; i < tabsContainer.length; i++) {
+            tabsContainer[i].classList.remove('shows__list--is-active');
+        }
+    }
+
+    //Secção Faq Acordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', AbreOuFechaResposta);
+    }
 })
 
-function removeBotaoAtivo () {
-    const buttons = document.querySelectorAll('[data-tab-button]');
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('shows__tabs__button--is-active')
-    }
-
+function ocultaElementosdoHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
 }
 
-
-function escondeAbas () {
-    const tabsContainer = document.querySelectorAll('[data-tab-id]');
-
-    for (let i = 0; i < tabsContainer.length; i++) {
-        tabsContainer[i].classList.remove('shows__list--is-active');
-    }
+function exibeElementosdoHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
 }
+
+function AbreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open'
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
+
